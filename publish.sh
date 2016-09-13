d1d8b2d500c3fc33a9f868184084133d5ef3876b
Kevin Kreiser <kevinkreiser@gmail.com>
2016-09-13 15:13:50 -0400

    forget vagrant and use the stuff we already have?!

--- publish.sh
+++ publish.sh
@@ -2,13 +2,14 @@
 set -e
 
 VERSION=$(cat version)
-RELEASES=( $(cat releases) )
 
 #get all of the packages ready
-NO_BUILD=true ./package.sh
+for dist in ${@}; do
+	NO_BUILD=true ./package.sh ${dist}
+done
 
 #have to have a branch of the code up there or the packages wont work from the ppa
-cd ${RELEASES[0]}/unpinned
+cd ${1}/unpinned
 bzr init
 bzr add
 bzr commit -m "Packaging for ${VERSION}-0ubuntu1."
@@ -16,9 +17,9 @@ bzr push --overwrite bzr+ssh://valhalla-routing@bazaar.launchpad.net/~valhalla-r
 cd -
 
 #sign and push each package to launchpad
-for release in ${RELEASES[@]}; do
+for dist in ${@}; do
 	for pin in pinned unpinned; do
-		debsign ${release}/${pin}/*source.changes
-		dput ppa:valhalla-routing/valhalla ${release}/${pin}/*source.changes
+		debsign ${dist}/${pin}/*source.changes
+		dput ppa:valhalla-routing/valhalla ${dist}/${pin}/*source.changes
 	done
 done
